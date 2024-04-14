bae45867eaff8ea49883995c8404ceb7a13ce762
Ken Lee <tklee1975@gmail.com>
2024-04-14 23:22:26 +0800

    Adding code for environment

--- src/views/test/TestSimple.js
+++ src/views/test/TestSimple.js
@@ -7,12 +7,19 @@ import {
 import React from 'react';
 
 export default function TestSimple() {
+    // Getting the environment variables
+    const { REACT_APP_NAME } = process.env;
+
     return (
         <Container maxWidth="lg">
             <Stack direction="column">
                 <Typography variant="h3">Test Simple</Typography>
 
                 <Typography variant="h4">Typography h4</Typography>
+                <Typography variant="p">
+                    AppName: {REACT_APP_NAME}
+                </Typography>
+
             </Stack>
         </Container>
     )
